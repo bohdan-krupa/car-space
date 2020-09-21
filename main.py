dfd2be617cd33f8b1ce75132f1a9090e49ba8850
Bohdan <krp@MacBook-Air-Bohdan.local>
2020-09-21 03:08:10 +0300

    intersects

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ execution_path = os.getcwd()
 
 detector = ObjectDetection()
 detector.setModelTypeAsYOLOv3()
-detector.setModelPath("./models/yolo.h5")
+detector.setModelPath('./models/yolo.h5')
 print(detector.modelPath)
 detector.loadModel()
 
 custom = detector.CustomObjects(car=True, bus=True, motorcycle=True, truck=True)
 detections = detector.detectCustomObjectsFromImage(
   custom_objects=custom,
-  input_image="frame.jpg",
-  output_image_path=os.path.join(execution_path , "result.jpg"),
+  input_image='frame.jpg',
+  output_image_path=os.path.join(execution_path , 'result.jpg'),
   minimum_percentage_probability=30
 )
 
 for eachObject in detections:
-  print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
+  print(f"{eachObject['name']} {round(eachObject['percentage_probability'])}% {eachObject['box_points']}")
--- screenshos.py
+++ screenshos.py
@@ -5,7 +5,7 @@ vidcap = cv2.VideoCapture('http://194.44.209.238/mjpg/video.mjpg')
 success, image = vidcap.read()
 
 while success:
-  cv2.imwrite("frame.jpg", image)    
+  cv2.imwrite('input.jpg', image)    
   success, image = vidcap.read()
   print('Read a new frame: ', success)
   time.sleep(10)
