0454bb63ba28360d096a5edbc2432a26577d3572
Bohdan <krp@MacBook-Air-Bohdan.local>
2020-10-03 01:04:57 +0300

    conn

--- bot/postgres_worker.py
+++ bot/postgres_worker.py
@@ -1,13 +1,18 @@
 import psycopg2
 
 try:
-    con = psycopg2.connect(
-        database="car-parking",
+    conn = psycopg2.connect(
+        database="krp",
         user="postgres",
         password="",
         host="127.0.0.1",
-        port="5432"
-    )mrRobot2205
+        port="5431"
+    )
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM accounts;")
+    records = cursor.fetchall()
+
+    print(records)
 except Exception as e:
     print(e)
-
