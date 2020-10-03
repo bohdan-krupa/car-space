982cc7d8f32728f92b514d63a0c90ac3729aa363
Danylo Melnyk <danylomelnyk@MacBook-Pro-Danylo.local>
2020-10-03 15:40:55 +0300

    postgres1.0

---
+++ bot/backupData/dump.sqlite.sql
@@ -0,0 +1,17 @@
+-- TABLE
+CREATE TABLE "parkings" (
+	"id"	INTEGER UNIQUE,
+	"street"	TEXT,
+	"spaces_amount"	INTEGER,
+  	"latitude"	REAL,
+	"longitude"	REAL,
+	"is_camera" BOOL,
+	PRIMARY KEY("id")
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
--- bot/postgres_worker.py
+++ bot/postgres_worker.py
@@ -1,13 +1,34 @@
-import psycopg2
+import json
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, text
+from sqlalchemy import Column, Integer, String, Date, REAL, Text
 
-try:
-    con = psycopg2.connect(
-        database="car-parking",
-        user="postgres",
-        password="",
-        host="127.0.0.1",
-        port="5432"
-    )mrRobot2205
-except Exception as e:
-    print(e)
+
+Base = declarative_base()
+engine = create_engine("postgresql://postgres@localhost:5431/postgres")
+
+class Parking(Base):
+    __tablename__ = "parking"
+    id = Column(Integer(), primary_key=True)
+    street = Column(String())
+    latitude = Column(REAL())
+    longitude = Column(REAL())
+    camera_url = Column(Text())
+
+    def datafromJson(self):
+        pass
+
+
+Base.metadata.create_all(engine)
+
+
+
+
+
+
+
+
+
+
+
 
