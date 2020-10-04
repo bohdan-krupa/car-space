852133679bfda5f751705253e3215c46954ae3bb
Danylo Melnyk <danylomelnyk@MacBook-Pro-Danylo.local>
2020-10-04 09:43:35 +0300

    don't have a clue

--- bot/postgres_worker.py
+++ bot/postgres_worker.py
@@ -1,7 +1,7 @@
 import json
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, text
-from sqlalchemy import Column, Integer, String, Date, REAL, Text
+from sqlalchemy import Column, Integer, String, Date, REAL, Text, BOOLEAN
 
 
 Base = declarative_base()
@@ -11,14 +11,30 @@ class Parking(Base):
     __tablename__ = "parking"
     id = Column(Integer(), primary_key=True)
     street = Column(String())
+    spaces_amount = Column(Integer())
     latitude = Column(REAL())
     longitude = Column(REAL())
-    camera_url = Column(Text())
+    camera_url = Column(BOOLEAN())
 
-    def datafromJson(self):
-        pass
+    @staticmethod
+    def datafromJson():
+        with open("./backupData/SQLite.json", "r") as file:
+            data = json.loads(file.read())
+        
+        values = [list(x.values()) for x in data]
+        print(values)
+
+                
 
 
+
+            
+                
+                
+           
+
+        
+Parking.datafromJson()
 Base.metadata.create_all(engine)
 
 
@@ -31,7 +47,3 @@ Base.metadata.create_all(engine)
 
 
 
-
-    print(records)
-except Exception as e:
-    print(e)
